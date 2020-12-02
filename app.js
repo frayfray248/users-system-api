8a56a387a7aa739634306ae1954e5fbc262c02c3
frayfray248 <nicman248@hotmail.com>
2020-12-01 16:15:48 -0800

    basic server setup

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const http = require('http');
+
+// port
+const port = process.env.PORT;
+
+// app object instances
+const app = express();
+const server = http.createServer(app);
+
+// http root
+app.use("/", (req, res) => {
+    res.send({ response: "I am alive"}).status(200);
+});
+
+// start server
+server.listen(port, () => console.log(`Listening on ${port}`));
