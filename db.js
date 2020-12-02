8cfdf9a7cededc51252f74e34e5e4b993a45f672
frayfray248 <nicman248@hotmail.com>
2020-12-01 17:29:38 -0800

    db connection console status

--- db.js
+++ db.js
@@ -4,3 +4,7 @@ mongoose.connect('mongodb://localhost/users-systems-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => { console.log("Successfully connected to db")});
