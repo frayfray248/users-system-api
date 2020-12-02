96241a16732fd8a3e6d9bf5cd6b92d37010cdc7f
frayfray248 <nicman248@hotmail.com>
2020-12-02 15:54:11 -0800

    User login with jwt token

--- api/controllers/UsersController.js
+++ api/controllers/UsersController.js
@@ -1,6 +1,7 @@
 // imports
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // db
 const db = mongoose.connection;
@@ -12,6 +13,62 @@ const User = require('../models/userModel');
 exports.getUsers = (req, res, next) => {
     (async () => {
         await res.send({ response: "users" }).status(200);
+    })();
+};
+
+// log a user in
+exports.logUserIn = (req, res, next) => {
+    (async () => {
+        try {
+            // find user
+            const user = await User.find({
+                email: req.body.email,
+                username: req.body.username
+            });
+
+            if (user.length < 1) throw new Error('Authorization failed');
+
+            // password check
+            const auth = await bcrypt.compare(req.body.password, user[0].password);
+
+            if (!auth) throw new Error('Authorization failed');
+
+            // json token
+            const token = await jwt.sign({
+                username: user[0].username,
+                email: user[0].email,
+                userId: user[0]._id
+            },
+                process.env.JWT_KEY,
+                {
+                    expiresIn: "1h"
+                }
+            );
+
+            // authorization successful
+            // send token to client
+            res.status(200).send({ 
+                message: "authorization successful",
+                token: token
+            })
+
+            // error handling
+        } catch (error) {
+
+            console.log(error);
+
+            const errorMessage = error.message;
+
+            // failed authorization error
+            if (errorMessage === 'Authorization failed') {
+                await res.status(401).send({ message: errorMessage })
+            }
+            // all other errors
+            else {
+                await res.status(500).send({ message: errorMessage });
+            }
+
+        }
     })();
 };
 
@@ -63,6 +120,9 @@ exports.addUser = (req, res, next) => {
             // existing email error
             else if (errorMessage === 'Email exists') {
                 await res.status(409).send({ message: errorMessage })
+                // failed authorization error
+            } else if (errorMessage === 'Authorization failed') {
+                await res.status(401).send({ message: errorMessage })
             }
             // all other errors
             else {
