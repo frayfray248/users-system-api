f98576227c8ce948d264b6a53e1eb3665109fb6e
frayfray248 <nicman248@hotmail.com>
2021-03-02 12:01:13 -0800

    cors

--- app.js
+++ app.js
@@ -31,6 +31,19 @@ app.use(morgon('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers', 
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // users route
 app.use('/users', usersRoute);
 
